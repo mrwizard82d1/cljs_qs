826d85bbd7a93c1e751856e71e8b4130e591e9b0
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-09-07 10:58:03 -0500

    Reduce the amount of "boilerplate" for the script.
    By specifying the main function as part of the build function, we reduce
    the amount of "boilerplate" text that we must include in files.
    Specifically, we eliminate the need to specifically include the
    goog/base.js file and to invoke a specific function on the page.

--- build.clj
+++ build.clj
@@ -5,7 +5,9 @@
 ;; Build all .cljs files in the "src" directory writing the result to the
 ;; JavaScript file "out.main.js."
 ;;
-;; Remember that the second argument to `build` is a map of options. In this
-;; particular script, the only option specifies the file to which to output
-;; the "compiled" JavaScript file.
-(cljs.build.api/build "src" {:output-to "out/main.js"})
+;; The second argument to `build` is a map of options. The `:main` option
+;; tells the compiler the function to automatically invoke. The `:output-to`
+;; option specifies the destination for the compiled file.
+(cljs.build.api/build "src"
+  {:main 'hello-world.core
+   :output-to "out/main.js"})

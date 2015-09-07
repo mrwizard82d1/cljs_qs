572eca8a70c00a0fe5dd313a6cb333058ee68362
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-09-07 13:22:24 -0500

    Write script to create a browser REPL.
    Write a Clojure script to create a browser REPL (brepl). Change the core
    to create the (one-time) connection to the browser. This change allows
    one to evaluate ClojureScript code in the context of the browser.
    
    In addition, this process creates an automatic build process that
    watches the source code directory for changes. This allows you to reload
    the namespace whenever you make changes and then evaluate newly added
    functions in the context of the browser.

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -1,5 +1,15 @@
-(ns hello_world.core)
+(ns hello-world.core
+  (:require [clojure.browser.repl :as repl]))
+
+;; Create the connection to the browser. By enclosing it in `defonce`, we ensure that we only construct the
+;; connection once even if we reload the namespace during development.
+(defonce conn
+  (repl/connect "http://localhost:9000/repl"))
 
 ;; The following lines "redirect" calls to `println` to the browser console.
 (enable-console-print!)
 (println "Hello ClojureScript QuickStart!")
+
+;; ADDED
+(defn foo [a b]
+  (+ a b))

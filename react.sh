73b76e00ceca9ce84f333fc8aa26eb0079994ef2
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-09-07 19:19:40 -0500

    Change ClojureScript to use React.

---
+++ react.sh
@@ -0,0 +1,1 @@
+java -cp cljs.jar:src:react-0.13.3-1.jar clojure.main build.clj
--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -1,13 +1,5 @@
 (ns hello-world.core
-  (:require [cljs.nodejs :as nodejs]))
+  (:require cljsjs.react))
 
-;; Print to the console using `println`.
-(nodejs/enable-util-print!)
-
-;; Node.js requires that we define a main function.
-(defn -main [& args]
-  (println "Hello QuickStart World on Node.js!"))
-
-;; And "refer" to the main function from a well-known variable.
-(set! *main-cli-fn* -main)
-
+(enable-console-print!)
+(println "Hello, React-ClojureScript World!")
